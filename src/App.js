d4332f41712a301e229ae30f7559a5b7c93cdd81
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-02-17 17:19:51 +0900

    코딩애플 | 이벤트리스너] React로 만드는 블로그 웹앱
    JS처럼 리액트에서도 이벤트 리스너를 사용할 수 있는데, onClick을 이용해서 이벤트를 걸 수 있다.
    onClick = "" 큰따옴표 안에는
    1. 직접만든함수명 ( )
        ex. 함수( )
    2. 화살표 함수
        ex. ( ) => { }
        ex ex. ( ) => { 데이터변경함수 ( 데이터명 + 1 ) }

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  let [글제목, 글제목변경] = useState("남자 코트 추천"); 
-  /* useState()를 사용하면 Array형태가 만들어지는데 그 안에 두 개의 데이터가 존재한다. 
-  [state데이터(남자 코트 추천), state 데이터 변경 함수] */
-  let [글제목2, 글제목변경2] = useState(["남자 코트 추천", "강남 우동 맛집"]); 
-  /* state는 array, object... 모두 넣어도 됨! 위 코드처럼 */
-    let [글제목3, 글제목변경3] = useState(["푸딩 종류", "설향 딸기 라떼 만들기", "촉촉한 딸기 쇼트 케이키"]); 
+  
+    let [글제목, 글제목변경함수] = useState(["푸딩 종류", "설향 딸기 라떼 만들기", "촉촉한 딸기 쇼트 케이키"]);
+    let [따봉, 따봉변경함수] = useState(0);
+    let [글변경, 글변경함수] = useState(["커스터드 케이키", "딸기 쇼트 푸딩", "설향 바나나"]);
   
 
-  let posts = "강남 고기 맛집";
   return (
     <div className="App">
       <div className='black-nav'>
@@ -23,23 +19,23 @@ function App() {
       </div>
 
       <div className='list'>
-      <h3> { 글제목3[0] } </h3>
+      <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경함수(따봉+1) } }>🧡</span> { 따봉 } </h3>
         <p>2023-02-13</p>
         <hr/>
       </div>
 
       <div className='list'>
-      <h3> { 글제목3[1] } </h3>
+      <h3> { 글제목[1] } </h3>
         <p>2023-02-14</p>
         <hr/>
       </div>
 
       <div className='list'>
-      <h3> { 글제목3[2] } </h3>
+      <h3> { 글제목[2] } </h3>
         <p>2023-02-15</p>
         <hr/>
       </div>
-      
+      <button onClick={ ()=> { 글제목변경함수(글변경) } }>글 변경</button>
     </div>
   );
 }
