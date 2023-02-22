cce27c7cb073fad77b4f9681d81e738d072a9d29
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-02-22 16:35:15 +0900

    코딩애플 | 부모 컨포넌트 state를 자식 컴포넌트에 가져와보자! props] React
    코딩애플 | 부모 컨포넌트 state를 자식 컴포넌트에 가져와보자! props] React로 만드는 블로그 웹앱
    
    전에 코딩애플 | Component로 긴 html 코드 치환기] React로 만드는 블로그 웹앱 에서
    다른 컴포넌트에서 만든 state를 사용하려면 복잡한 절차를 거쳐야 한다며 props에 대해 언급한 적이 있는데, 지금이 바로 그 props에 관한 이야기다!
      
    부모 컴포넌트(여기서는 App 컴포넌트)에서 만든 state를 자식 컴포넌트에 가져와서 사용이 가능한데!
    부모 컴포넌트 ---state---전송---props를---통해서---> 자식 컴포넌트 (이렇게만 가능하다!)
    자식 컴포넌트 state -> 부모 컴포넌트(❌)
    자식 -> / <- 자식(❌) = 같은 말 = 옆집 자식 -> / <- 옆집 자식 (❌)
    
    props의 사용법 ①, ②, ③, ★
    ① 자식 컴포넌트를 만들어서 대체했던 나만의 diy html 태그에 속성처럼
         작명 = { 부모 컴포넌트에서 가져오고 싶은 state이름 } 
         을 작성한다. 관습적?으로 왼쪽 이름을 오른쪽의 state이름과 동일하게 작성한다고 한다.
         ex. <Modal state이름 = { state 이름 } />
    ② 자식 컴포넌트로 가서 파라미터(매개변수)를 아무 이름으로 부여해준다. 즉 파라미터를 넣어주고 이름은 자유! 관습적으로 파라미터 이름은 props라고 부여한다고 한다.
         ex. function Modal (props) { ~생략~ }
    ③ 자식 컴포넌트의 return ( ) 안의 html 태그 안에 
         ②에서 지정한 파라미터의 이름 + . + ① 에서 작명한 속성 왼쪽 이름
         을 써주면 된다!
         ex. <h3> { props.글제목 } </h3> => 중괄호를 쓰는 건 데이터 바인딩의 규칙인 { 변수명 } 이것 때문이겠지?!
    ★ 참고로 ③의 예시이자 커밋한 코드에도 있었던 글제목의 n번째 아이템을 소환하고 싶다면 => 부모 컴포넌트 state의 array의 아이템을 소환하고 싶다면
        { props.글제목[n] } => 이렇게 array 아이템 불러오듯이 코딩하면 된다. 글제목이라는 것 자체가 array이기 때문이다!
    
    🍮 위대한 나의 발견!!! (props의 순서와 비교해보자!! 대칭으로 맞춰서 설명해놓음!)
    - 해당 강의 맨 마지막에 props의 특성을 살려 모달창의 배경 색을 바꾸는 것에 대한 이야기가 나와서 멈춰놓고, 이번 시간에 배웠던 props의 속성을 생각하며
    ① 제목 변경 함수에 let으로 배경색 아이템을 써놓은 array를 생성한 다음 => props는 state 뿐만 아니라 함수명이나 이것 저것 모든 걸 다 가져올 수 있다고 들었다!!
    ①-1 자식 컴포넌트 나만의 diy html 태그 안에 속성으로 color={color} 를 집어넣고
    ② 자식 컴포넌트의 파라미터에 props라는 이름을 부여한 다음
    ③  style={{ background: props.color[1] }} 를 작성하면 끝!!
    style과 같은 태그 속성을 props로 가져올 때에는 중괄호를 두 개 써주는 것을 잊지 말자!! style 속성은 중괄호를 늘 2개!! style = {{ 속성 : 속성값 }}
    나는 위와 같은 방식으로 배경색을 변하게 코딩해보았다! 지금은 color array의 아이템을 가져올 때 1로 임의로 골랐지만 랜덤으로도 바꿀 수 있도록 설계를 해보고 싶다.
    그때는 i라는 변수를 이용해서 랜덤 모달창 배경색을 만들 수 있지 않을까?

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() { /* 이 app function도 하나의 컴포넌트이기 때문에 �
     let [글제목, 글제목변경함수] = useState(["푸딩 종류", "설향 딸기 라떼 만들기", "촉촉한 딸기 쇼트 케이키"]);
     let [따봉, 따봉변경함수] = useState([0, 0, 0]);
     let [글변경, 글변경함수] = useState(["커스터드 케이키", "딸기 쇼트 푸딩", "설향 바나나"]);
-  let [modal, setModal] = useState(false); /* 여기에는 현재 상태를 적어주는 게 좋다고 했지만, false로 하신 걸 보니 주로 처음 웹페이지에 들어갔을 때 보였으면 좋겠는 상태를 적는 게 좋은 것 같다! 닫힘 열림 외에도 0이나 1, true나 false와 같이 상태를 표현할 수 있는 말이면 다 되는 것 같다! */
+    let [modal, setModal] = useState(false); /* 여기에는 현재 상태를 적어주는 게 좋다고 했지만, false로 하신 걸 보니 주로 처음 웹페이지에 들어갔을 때 보였으면 좋겠는 상태를 적는 게 좋은 것 같다! 닫힘 열림 외에도 0이나 1, true나 false와 같이 상태를 표현할 수 있는 말이면 다 되는 것 같다! */
+  let [color, setcolor] = useState(["orange", "pink", "skygreen"])
   
 
   function 제목변경() {
@@ -20,6 +21,7 @@ function App() { /* 이 app function도 하나의 컴포넌트이기 때문에 �
   }
   
   function likeItCount() {
+    let i = [0, 1, 2];
     let like = [...따봉];
     like[i] = like[i] + 1;
     따봉변경함수( like )
@@ -50,16 +52,12 @@ function App() { /* 이 app function도 하나의 컴포넌트이기 때문에 �
         <hr/>
       </div> */}
       
-      <button onClick={ 제목변경 }>글 변경</button>
 
-      {
-        modal == true ? <Modal /> : null
-      }
 
       {
        글제목.map(function (a, i){
          return (<div className='list' key={i}>
-          <h3 /* onClick={() => { setModal(!modal) }} */> { a }
+          <h3 onClick={() => { setModal(!modal) }}> { a }
              <span onClick={() => {
                let like = [...따봉];
                 like[i] = like[i] + 1;
@@ -74,16 +72,24 @@ function App() { /* 이 app function도 하나의 컴포넌트이기 때문에 �
         })
       }
 
+            <button onClick={ 제목변경 }>글 변경</button>
+
+      {
+        modal == true ? <Modal 글제목={글제목} color={color}
+          제목변경={제목변경} /> : null
+      }
+
     </div>
   );
 }
 
-function Modal() { /* 리액트 특징 7. 긴 html 코드를 component를 이용해서 축약 및 치환할 수 있다! => 관리가 편해진다. 그렇지만 뭐든 과하면 복잡하듯, 많이 만들면 관리가 점점 더 어려워지니 꼭 필요할 때만 만들기를!*/
+function Modal(props) { /* 리액트 특징 7. 긴 html 코드를 component를 이용해서 축약 및 치환할 수 있다! => 관리가 편해진다. 그렇지만 뭐든 과하면 복잡하듯, 많이 만들면 관리가 점점 더 어려워지니 꼭 필요할 때만 만들기를!*/
   return (
-    <div className='modal'>
-        <h2>제목</h2>
+    <div className='modal' style={{ background: props.color[1] }}>
+      <h2>{ props.글제목[0] }</h2>
         <p>날짜</p>
-        <p>상세내용</p>
+      <p>상세내용</p>
+      <button onClick={ props.제목변경 }>글 제목 변경</button>
     </div>
   )
 }
